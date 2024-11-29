0b8eaf1b32ff5d8c86f0210eee033c0da9ad85e1
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-11-29 10:54:47 +0100

    Initial Commit

---
+++ SwiftUIProject3-Restart/SwiftUIProject3_RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject3_RestartApp.swift
+//  SwiftUIProject3-Restart
+//
+//  Created by Yaro Paul  on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProject3_RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIProject3-RestartTests/SwiftUIProject3_RestartTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProject3_RestartTests.swift
+//  SwiftUIProject3-RestartTests
+//
+//  Created by Yaro Paul  on 29/11/2024.
+//
+
+import Testing
+@testable import SwiftUIProject3_Restart
+
+struct SwiftUIProject3_RestartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
